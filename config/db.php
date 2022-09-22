5984639ab604aa8298c48e74af22947a52df7a86
xLasTx <xlastx.qq@gmail.com>
2022-09-22 20:47:48 +0300

    Add docker

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=realtor',
+    'dsn' => 'mysql:host=database;dbname=main',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
