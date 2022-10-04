d8986941a8c35dd4445f2c4864078d75738f4bf1
xLasTxqq <xlastx.qq@gmail.com>
2022-10-04 11:37:09 +0300

    Add migrations

---
+++ migrations/m221004_072719_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m221004_072719_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'login' => $this->string(255)->unique()->notNull(),
+            'password' => $this->string(255)->notNull(),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%users}}');
+    }
+}
---
+++ migrations/m221004_080651_create_applications_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%applications}}`.
+ */
+class m221004_080651_create_applications_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%applications}}', [
+            'id' => $this->primaryKey(),
+            'full_name' => $this->string(),
+            'phone_number' => $this->string(),
+            'client_comment' => $this->text(),
+            'status' => $this->string()->defaultValue('new')->notNull(),
+            'date_meeting' => $this->timestamp(),
+            'manager_comment' => $this->text(),
+            'date_of_purchase' => $this->timestamp(),
+            'appartment_id' => $this->integer()->notNull(),
+            'created_at' => $this->timestamp(), 
+            'updated_at' => $this->timestamp(),
+        ]);
+
+        $this->createIndex(
+            'idx-applications-appartment_id',
+            'applications',
+            'appartment_id'
+        );
+
+        $this->addForeignKey(
+            'fk-applications-appartment_id',
+            'applications',
+            'appartment_id',
+            'appartments',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk-applications-appartment_id',
+            'applications'
+        );
+
+        $this->dropIndex(
+            'idx-applications-appartment_id',
+            'applications'
+        );
+
+        $this->dropTable('{{%applications}}');
+    }
+}
